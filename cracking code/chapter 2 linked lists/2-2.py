d2992b246b99ca667c18a74c671d1e7c460db153
Ashton Smith <ashton23@pdx.edu>
2021-10-04 14:58:53 -0700

    chapter 2 init commit

---
+++ cracking code/chapter 2 linked lists/2-2.py
@@ -0,0 +1,2 @@
+# Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
+# Hints:  # 8, #25, #41, #67, #126
---
+++ cracking code/chapter 2 linked lists/2-6.py
@@ -0,0 +1,2 @@
+# Palindrome: Implement a function to check if a linked list is a palindrome.
+# Hints:  # 5, #13, #29, #61, #101
