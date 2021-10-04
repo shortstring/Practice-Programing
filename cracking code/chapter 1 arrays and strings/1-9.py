5714f2657ad5e402af9a45084b823694e78290e2
Ashton Smith <ashton23@pdx.edu>
2021-10-04 14:51:59 -0700

    1-9 complete

--- cracking code/chapter 1 arrays and strings/1-9.py
+++ cracking code/chapter 1 arrays and strings/1-9.py
@@ -2,3 +2,27 @@
 # of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
 # call to isSubstring(e.g., "waterbottle" is a rotation of"erbottlewat").
 # Hints:  # 34, #88, # 7 04
+
+
+def is_substring(str1, str2):
+    temp = ""  # used to hold rotated str2
+    if(len(str1) != len(str2)):
+        return False
+    for i, char1 in enumerate(str1):
+        for j, char2 in enumerate(str2):
+            # first char found?
+            if(char1 == char2):
+                temp = str2[j:len(str2)] + str2[0:j]
+                if temp == str1:
+                    return True
+    return False
+
+
+def main():
+    str1 = "waterbottle"
+    str2 = "terbottlewa"
+    print(is_substring(str1, str2))
+    return 0
+
+
+main()
