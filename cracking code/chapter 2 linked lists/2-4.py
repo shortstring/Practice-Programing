827b5b20f1da9b4ddbaab37aea49e3cd7145f8a6
Ashton Smith <ashton23@pdx.edu>
2021-10-08 13:18:22 -0700

    2-4 complete

--- cracking code/chapter 2 linked lists/2-4.py
+++ cracking code/chapter 2 linked lists/2-4.py
@@ -16,6 +16,7 @@
 # Output:
 # 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
 # Hints: #3, #24
+
 def main():
     list = LinkedList()
     list.head = Node(3)
@@ -28,7 +29,7 @@ def main():
 
     print('before')
     list.print_list()
-    list.partition_list(5)
+    list.partition_list(3)
     print('after')
     list.print_list()
     return 0
@@ -60,33 +61,66 @@ class LinkedList():
     def partition_list(self, x):
         if not self.head:
             return None
-        return self.partition_list_r(self.head, x)
 
-    def partition_list_r(self, head, x):
-        # case 1: not head
-        if not head:
-            return None
-        # case 2: check if head value is  greater than x
-        elif head.data > x:
-            if head.next and head.next.data < x:
-                temp = head
-                head = head.next
-                head.next = temp
-            # if head < x
-            # then if head.next < x
-            #   then swap nodes
-            # '''
-        # case 3: head == x
-        elif head.data == x:
-            if head.next and head.next.data < x:
-                temp = head
-                head = head.next
-                head.next = temp
-        # case 4: head < x
+        low_list = LinkedList()
+        low_head = None
+        low_tail = None
+
+        high_list = LinkedList()
+        high_head = None
+
+        head = self.head
+        while head:
+            if head.data >= x:
+                if not high_head:
+                    high_head = Node(head.data)
+                else:
+                    temp = Node(head.data)
+                    temp.next = high_head
+                    high_head = temp
+            else:
+                if not low_head:
+                    low_head = Node(head.data)
+                    low_tail = low_head
+                else:
+                    temp = Node(head.data)
+                    temp.next = low_head
+                    low_head = temp
+            head = head.next
+        if not low_head:
+            self.head = high_head
         else:
-            pass
-        head.next = self.partition_list_r(head.next, x)
-        return head
+            low_tail.next = high_head
+            self.head = low_head
+
+    # attempt to solve recursivley...
+        # gave up and solved iteravely because i dont know how to return the high and low list
+
+    # def partition_list_r(self, head, low_list, high_list, x):
+    #     # case 1: not head
+    #     if not head:
+    #         return None
+    #     # case 2: check if head value is  greater/equal than x
+    #     elif head.data >= x:
+    #         high_list = self.add_to_head(high_list.head, head.data)
+    #     # case 3: head < x
+    #     else:
+    #         low_list = self.add_to_head(low_list.head, head.data)
+
+    # def add_to_head(self, head, to_add):
+    #     if(not head):
+    #         head = Node(to_add)
+    #     else:
+    #         temp = Node(to_add)
+    #         temp.next = head
+    #         head = temp
+    #     return head
+
+    # def combine_lists(self, head_one, head_two):
+    #     if not head_one or not head_one.next:
+    #         return head_two
+    #     head_one.next = self.combine_lists(self, head_one.next, head_two)
+    #     return head_one
 
 
 main()
