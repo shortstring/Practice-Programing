5a95017060772a0c2dccf1a7978ff71b9b446e6d
Ashton Smith <ashton23@pdx.edu>
2021-10-15 17:19:09 -0700

    chapter 4 init

---
+++ cracking code/chapter 4 trees and graphs/4-2.py
@@ -0,0 +1,2 @@
+# Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+# Hints: #79, #73, #7 76
---
+++ cracking code/chapter 4 trees and graphs/4-9.py
@@ -0,0 +1,6 @@
+# 4.9 BST Sequences: A binary search tree was created by traversing through an array from left to right
+# and inserting each element. Given a binary search tree with distinct elements, print all possible
+# arrays that could have led to this tree.
+# Input:
+# Output: {2, 1, 3}, {2, 3, 1}
+# Hints: #39, #48, #66, #82
