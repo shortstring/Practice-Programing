09d4730132dcda009b4ece5939056bb73ac87929
Ashton Smith <ashton23@pdx.edu>
2021-10-28 12:14:24 -0700

    spacing fixed

--- react lab/react-todo/src/components/Todo.js
+++ react lab/react-todo/src/components/Todo.js
@@ -5,7 +5,7 @@ import Backdrop from './Backdrop';
 
 // any java script expression goes between {}, treated as javascript, any single line, no blocks.. like if statements.
 function Todo(props) {
-    const [modalIsOpen, setModalIsOpen] = useState(false); //creates a state - returns an array with two elements
+    const [ modalIsOpen, setModalIsOpen] = useState(false);//creates a state - returns an array with two elements
 
     function deleteHandler() {
         setModalIsOpen(true);
@@ -14,28 +14,21 @@ function Todo(props) {
     function closeModalHandler() {
         setModalIsOpen(false);
     }
-
     function closeModalDelete() {
         props.Todo.
         setModalIsOpen(false);
     }
-    return ( <
-            div className = "card" >
-            <
-            h2 > { props.text } < /h2> <
-            div >
-            <
-            button id = "btn"
-            onClick = { deleteHandler } > Delete < /button> <
-            /div> {
-                modalIsOpen && < Modal onCancel = { closeModalHandler }
-                onConfirm = { closeModalDelete }
-                />} {
-                    modalIsOpen && < Backdrop onClick = { closeModalHandler }
-                    />} <
-                    /div>
-                );
-            }
+    return (
+        <div className="card">
+            <h2>{props.text}</h2>
+            <div>
+                <button id="btn" onClick={deleteHandler}>Delete</button>
+            </div>
+            {modalIsOpen && <Modal onCancel={closeModalHandler}  onConfirm={closeModalDelete}/>}
+            {modalIsOpen && <Backdrop onClick={closeModalHandler}/>}
+        </div>
+    );
+}
 
-            //name SHOULD start with a capital letter. (custom elements..)
-            export default Todo;
+//name SHOULD start with a capital letter. (custom elements..)
+export default Todo;
